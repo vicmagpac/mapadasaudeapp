3c168edd9a39eb3dbf5ffb02c0fe24ca4a22499e
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-11-16 08:33:40 -0300

    add line comments

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+// AppRegistry register
 AppRegistry.registerComponent(appName, () => App);
